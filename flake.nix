7941565534932aae8f39729486ca891f0902f084
Kachi <108186213+PokumeKachi@users.noreply.github.com>
2026-01-31 17:34:47 +0700

    feat: replaced shell.nix with flake.nix

---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+    description = "description";
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    outputs =
+        { self, nixpkgs }:
+        let
+            systems = [ "x86_64-linux" ];
+            forAllSystems = nixpkgs.lib.genAttrs systems;
+
+            perSystem =
+                system:
+                let
+                    pkgs = import nixpkgs { inherit system; };
+                    libs = with pkgs; [
+                        wayland
+                        wayland-protocols
+                        libxkbcommon
+                        libinput
+                        seatd
+                        systemd
+                        libgbm
+                        libdrm
+                        libglvnd
+                        libGL
+                        libdisplay-info_0_2
+                        pixman
+                        (systemd.dev)
+                    ];
+                    tools = with pkgs; [
+                        pkg-config
+                    ];
+                    common = {
+                        buildInputs = libs;
+                        nativeBuildInputs = tools;
+                    };
+                in
+                {
+                    inherit
+                        pkgs
+                        libs
+                        tools
+                        common
+                        ;
+                };
+        in
+        {
+            packages = forAllSystems (
+                system:
+                let
+                    ps = perSystem system;
+                in
+                {
+                    default = ps.pkgs.rustPlatform.buildRustPackage (
+                        ps.common
+                        // {
+                            pname = "package-name";
+                            version = "0.1.0";
+                            src = ps.pkgs.lib.cleanSource ./.;
+                            cargoLock.lockFile = ./Cargo.lock;
+                        }
+                    );
+                }
+            );
+
+            devShells = forAllSystems (
+                system:
+                let
+                    ps = perSystem system;
+                in
+                {
+                    default = ps.pkgs.mkShell (
+                        ps.common
+                        // {
+                            shellHook = ''
+                                export SHELL=${ps.pkgs.bashInteractive}/bin/bash
+                                export LD_LIBRARY_PATH=${ps.pkgs.lib.makeLibraryPath ps.libs}:$LD_LIBRARY_PATH
+                            '';
+                        }
+                    );
+                }
+            );
+        };
+}
